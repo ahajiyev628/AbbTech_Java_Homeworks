7e6365165fdb0497ceda298231e926e2ca07c18b
ahajiyev628 <66174474+ahajiyev628@users.noreply.github.com>
2022-09-04 14:07:05 +0400

    Update FamilyDao.java

--- src/main/java/homework9/FamilyDao.java
+++ src/main/java/homework9/FamilyDao.java
@@ -1,6 +1,9 @@
 package homework9;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public interface FamilyDao {
     List<Family> getAllFamilies();
@@ -8,4 +11,17 @@ public interface FamilyDao {
     Boolean deleteFamily(int index);
     Boolean deleteFamily(Family f);
     List<Family> saveFamily(Family f);
+    List<Family> displayAllFamilies();
+    List<Family> getFamiliesBiggerThan(int n);
+    List<Family> getFamiliesLessThan(int n);
+    int countFamiliesWithMemberNumber();
+    void createNewFamily(Human father, Human mother, List<Human> children);
+    void deleteFamilyByIndex(int index);
+    Family deleteAllChildrenOlderThen(Family f, int n);
+    void bornChild(Family family, String gender);
+    Family adoptChild(Family f, Human child) ;
+    int count() ;
+    Family getFamilyById(int id);
+    List<Pet> getPets(int family_index, int index);
+    void addPet(int index, Pet p);
 }
